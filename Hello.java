22d7fe2e2be0c3d7d81f069def84d05661a69aae
sagar-dm-16 <sagardm2003@gmail.com>
2026-04-29 16:21:12 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 // Hello.java
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins!...");
+        System.out.println("Hello from Jenkins!");
     }
 }
